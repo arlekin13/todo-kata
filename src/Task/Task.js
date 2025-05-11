584c0385c8718eef53e11c7582ee1f2741912f59
arlekin13 <arlekin13rus@gmail.com>
2025-05-11 19:13:17 +0300

    Initial commit

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,36 +1,47 @@
 import React from 'react';
-import { formatDistanceToNow } from 'date-fns';
+
 
 import './Task.css'
 
-function Task({ task }) {
-  const { description, completed, createdAt} = task;
-
+function Task() {
+  
   return (
-
-    <ul className='todo-list'>
-      <li className={task.completed ? 'completed' : ''}>
-    <div className="view">
-      <input 
-      className="toggle" 
-      type="checkbox" 
-      checked={completed} />
-
-      <label>
-        <span className='description'> 
-        {description}
-          </span>
-          <span className="created">
-        created {formatDistanceToNow(createdAt, { addSuffix: true })}
-          </span>
-         </label>
-      
-      <button className="icon icon-edit"></button>
-      <button className="icon icon-destroy"></button>
-    </div>
-  </li>
-  </ul>
-    
+    <>
+      <li className="completed">
+        <div className="view">
+          <input className="toggle" type="checkbox" />
+          <label>
+            <span className="description">Completed task</span>
+            <span className="created">created 17 seconds ago</span>
+          </label>
+          <button className="icon icon-edit"></button>
+          <button className="icon icon-destroy"></button>
+        </div>
+      </li>
+      <li className="editing">
+        <div className="view">
+          <input className="toggle" type="checkbox" />
+          <label>
+            <span className="description">Editing task</span>
+            <span className="created">created 5 minutes ago</span>
+          </label>
+          <button className="icon icon-edit"></button>
+          <button className="icon icon-destroy"></button>
+        </div>
+        <input type="text" className="edit" value="Editing task" />
+      </li>
+      <li>
+        <div className="view">
+          <input className="toggle" type="checkbox" />
+          <label>
+            <span className="description">Active task</span>
+            <span className="created">created 5 minutes ago</span>
+          </label>
+          <button className="icon icon-edit"></button>
+          <button className="icon icon-destroy"></button>
+        </div>
+      </li>
+    </>
   );
 }
 
--- src/TaskList/TaskList.js
+++ src/TaskList/TaskList.js
@@ -4,17 +4,13 @@ import './TaskList.css'
 
 function TaskList() {
   
-  const tasks = [
-    { id: 1, description: 'Завершить задание', completed: false, createdAt: new Date() },
-    { id: 2, description: 'Проверить код', completed: true, createdAt: new Date() },
-    { id: 3, description: 'Отправить решение', completed: false, createdAt: new Date() },
-  ];
 
+
+  
   return (
-    <ul className="todo-list">
-      {tasks.map(task => (
-        <Task key={task.id} task={task} />
-      ))}
+<ul className="todo-list">
+      <Task /> 
+    
     </ul>
   );
 }
